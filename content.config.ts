e52263f4d3e75e008942cb11ee92ea9938897075
Sanjay Soundarajan <sanjay.16@live.com>
2025-02-06 13:01:36 -0800

    wip: fix hmr issue

--- content.config.ts
+++ content.config.ts
@@ -2,10 +2,10 @@ import { defineContentConfig, defineCollection } from "@nuxt/content";
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
-      source: "**/*.md",
-      type: "page",
-    }),
+    // content: defineCollection({
+    //   source: "**/*.md",
+    //   type: "page",
+    // }),
     docs: defineCollection({
       source: "docs/**/*.md",
       type: "page",
